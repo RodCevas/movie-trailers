22e0f4059df0caa108512e2c2c66b7a2d577d252
rodcevas <60941287+RodCevas@users.noreply.github.com>
2025-02-04 15:43:15 -0300

    angular schematics config

--- src/app/shared/components/header/aaaaaa/aaaaaa.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-aaaaaa',
-  imports: [],
-  templateUrl: './aaaaaa.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class AaaaaaComponent { }
