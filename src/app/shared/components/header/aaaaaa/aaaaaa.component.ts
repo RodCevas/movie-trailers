01f11149d7023a90ccf7f217a1c585dd08854b59
rodcevas <60941287+RodCevas@users.noreply.github.com>
2025-02-04 11:51:49 -0300

    navbar

---
+++ src/app/shared/components/header/aaaaaa/aaaaaa.component.ts
@@ -0,0 +1,9 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-aaaaaa',
+  imports: [],
+  templateUrl: './aaaaaa.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AaaaaaComponent { }
