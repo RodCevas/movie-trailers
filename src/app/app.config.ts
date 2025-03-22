82351bb80cc92242f8d3559715c1db7ffbd865a8
rcevas <60941287+RodCevas@users.noreply.github.com>
2025-03-22 13:24:46 +0100

    header routing

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,9 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  provideRouter,
+  withInMemoryScrolling,
+  withRouterConfig,
+} from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -7,7 +11,12 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        scrollPositionRestoration: 'enabled', // enable position restoration
+      })
+    ),
     provideHttpClient(),
   ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './feature/home/home.component';
 import { DetailComponent } from './shared/components/detail/detail.component';
+import { LatestTrailersComponent } from './feature/latest-trailers/latest-trailers.component';
+import { TrendingTrailersComponent } from './feature/trending-trailers/trending-trailers.component';
+import { PageNotFoundComponent } from './feature/page-not-found/page-not-found.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'latest', component: LatestTrailersComponent },
+  { path: 'trending', component: TrendingTrailersComponent },
   {
     path: 'detail/:id',
     component: DetailComponent,
   },
+  {path: '**', component: PageNotFoundComponent}
 ];
