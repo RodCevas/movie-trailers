fcc3d7b5d0924bd50cad6d091037bd1b94b9975e
Rodrigo Cevas <rodrigo.cevas@in2.es>
2025-03-28 11:44:14 +0100

    search bar

--- src/app/core/services/data.service.ts
+++ src/app/core/services/data.service.ts
@@ -1,9 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
 import { environment } from '../../../environments/environment';
-import { Movie } from '../models/models';
 
 @Injectable({
   providedIn: 'root',
@@ -16,34 +14,6 @@ export class DataService {
   // GET all items
   getAll(type: string = '', param: string = ''): Observable<any[]> {
     return this.http.get<any[]>(this.apiUrl + type + '?language=en' + param);
-  }
-
-  // GET item by ID
-  getById(id: number, type: string = ''): Observable<any> {
-    return this.http
-      .get<any>(`${this.apiUrl}${type}?language=en&tmdb_id=${id}`)
-      .pipe(catchError(this.handleError));
-  }
-
-  // POST (create) item
-  create(item: any): Observable<any> {
-    return this.http
-      .post<any>(this.apiUrl, item)
-      .pipe(catchError(this.handleError));
-  }
-
-  // PUT (update) item
-  update(id: number, item: any): Observable<any> {
-    return this.http
-      .put<any>(`${this.apiUrl}/${id}`, item)
-      .pipe(catchError(this.handleError));
-  }
-
-  // DELETE item
-  delete(id: number): Observable<void> {
-    return this.http
-      .delete<void>(`${this.apiUrl}/${id}`)
-      .pipe(catchError(this.handleError));
   }
 
   // Error handling
--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,17 +1,77 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  signal,
+} from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { DataService } from '../../../core/services/data.service';
+import { Movie } from '../../../core/models/models';
+import { CardComponent } from '../card/card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule, FormsModule, CardComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
+  movies = signal<Movie[]>([]);
+  filteredMovies = signal<Movie[]>([]);
+  error = signal<string>('');
 
   menuOpen = false;
 
+  inputText: string = '';
+
+  private searchSubject = new Subject<string>();
+
+  constructor(private dataService: DataService) {
+    this.searchSubject
+      .pipe(debounceTime(300), distinctUntilChanged())
+      .subscribe((query) => {
+        this.search(query);
+        this.inputText = query;
+      });
+  }
+
+  ngOnInit(): void {
+    this.getMovies();
+  }
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
+  }
+
+  getMovies() {
+    this.dataService.getAll('', '&limit=100').subscribe({
+      next: (data) => this.movies.set(Object.values(data)),
+      error: (err) => (this.error = err),
+    });
+  }
+
+  onSearch(event: Event): void {
+    const query = (event.target as HTMLInputElement).value;
+    this.searchSubject.next(query);
+  }
+  search(query: string): void {
+    query = (query || '').toString().toLowerCase();
+    if (query != '') {
+      this.filteredMovies.set(
+        this.movies().filter((movie) =>
+          (movie.title || '').toString().toLowerCase().includes(query)
+        )
+      );
+    } else {
+      this.filteredMovies.set([]);
+    }
+    /* window.scrollTo(0, 0); */
+  }
+
+  clearInput() {
+    this.inputText = '';
   }
 }
